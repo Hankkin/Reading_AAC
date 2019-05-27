a9d092b403a2d3c55620ed608cb87480fe3e5fcc
huanghaijie <huanghaijie@58ganji.com>
2019-05-27 18:44:14 +0800

    init

---
+++ app/src/main/java/com/hankkin/reading_aac/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.hankkin.reading_aac
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hankkin.reading_aac.ui.home.HomeFragment
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, HomeFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/hankkin/reading_aac/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.hankkin.reading_aac.ui.home
+
+import android.arch.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
