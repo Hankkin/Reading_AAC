3e6352c1c396fdce49ef8afe573bd185edba3e4e
huanghaijie <huanghaijie@58ganji.com>
2019-05-29 20:11:32 +0800

    init

--- app/src/main/java/com/hankkin/reading_aac/MainActivity.kt
+++ app/src/main/java/com/hankkin/reading_aac/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.hankkin.reading_aac
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var liveStr: MutableLiveData<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        liveStr = MutableLiveData<String>()
+        liveStr.observeForever {
+            Log.e("Hankkin", liveStr.value + lifecycle.currentState)
+        }
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        liveStr.value = "start"
+    }
+
+    override fun onResume() {
+        super.onResume()
+        liveStr.value = "resume"
+    }
+
+    override fun onPause() {
+        super.onPause()
+        liveStr.value = "pause"
+    }
+
+    override fun onStop() {
+        super.onStop()
+        liveStr.value = "stop"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        liveStr.value = "destroy"
+    }
+
+
 }
--- app/src/main/java/com/hankkin/reading_aac/ui/home/HomeViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.hankkin.reading_aac.ui.home
-
-import android.arch.lifecycle.ViewModel
-
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
